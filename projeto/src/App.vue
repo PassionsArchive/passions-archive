a9a1e9e8108eee05e586d71757446d28590e60ff
pietralavina <pietragiovana2008@gmail.com>
2025-10-21 11:42:07 -0300

    feat: add FooterComponent and update App.vue layout

--- projeto/src/App.vue
+++ projeto/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { ref } from 'vue';
-import HeaderComponent from './assets/components/HeaderComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 </script>
 
 <template>
   <HeaderComponent />
+
+  <FooterComponent />
 </template>
 
 <style scoped>
---
+++ projeto/src/components/FooterComponent.vue
@@ -0,0 +1,51 @@
+<script setup></script>
+
+<template>
+  <div>
+    <img src="/public/Group 6.png" alt="Cenas de Filmes" />
+    <nav>
+      <ul>
+        <li><a href="#">INÍCIO</a></li>
+        <li><a href="#">SALVOS</a></li>
+        <li><a href="#">COMO ESTÁ SEU HUMOR?</a></li>
+        <li><a href="#">ASSISTIDOS</a></li>
+        <li><a href="#">PERFIL</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+div {
+  background-color: #401818;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 40px 80px;
+  color: white;
+  gap: 40px;
+}
+nav {
+  width: 100%;
+  text-align: center;
+}
+
+nav ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+
+nav ul li a {
+  text-decoration: none;
+  color: white;
+  font-weight: 400;
+}
+
+nav ul li a:active{
+  font-weight: bold;
+}
+</style>
