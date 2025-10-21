94f89b14efc26973053bc15d4986415425a67b2b
pietralavina <pietragiovana2008@gmail.com>
2025-10-21 11:03:54 -0300

    feat: add HeaderComponent and update App.vue
    - Introduced HeaderComponent for navigation with links to various sections.
    - Updated App.vue to include the new HeaderComponent.
    - Removed unused logo.svg file from assets.
    - Enhanced main.css with global styles for a consistent layout and background.

--- projeto/src/App.vue
+++ projeto/src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
+import { ref } from 'vue';
+import HeaderComponent from './assets/components/HeaderComponent.vue';
 </script>
 
 <template>
+  <HeaderComponent />
 </template>
 
 <style scoped>
---
+++ projeto/src/components/HeaderComponent.vue
@@ -0,0 +1,55 @@
+<script setup>
+</script>
+
+<template>
+ <nav>
+   <ul>
+     <li><a href="#">INÍCIO</a></li>
+     <li><a href="#">SALVOS</a></li>
+     <li><a href="#">COMO ESTÁ SEU HUMOR?</a></li>
+     <li><a href="#">ASSISTIDOS</a></li>
+     <li><a href="#">PERFIL</a></li>
+   </ul>
+ </nav>
+</template>
+
+<style scoped>
+nav {
+ margin-top: 5vw;
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ padding: 6px 0;
+ border-top: 2px solid black;
+ border-bottom: 2px solid black;
+ height: 10vh;
+}
+
+ul {
+ list-style: none;
+ display: flex;
+ gap: 40px;
+ padding: 0;
+ margin: 0;
+}
+
+li a {
+ text-decoration: none;
+ font-size: 1rem;
+ color: black;
+ padding: 4px 10px;
+ border: 1.5px solid transparent;
+ transition: all 0.3s ease;
+ border-radius: 2px;
+}
+
+li a.active {
+ border: 1.5px solid black;
+ border-radius: 2px;
+}
+
+li a:hover {
+ border: 1.5px solid black;
+ border-radius: 2px;
+}
+</style>
